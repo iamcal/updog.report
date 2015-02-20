dfd70f690696fb630f7b0041ddaef5f2f40a92d6
Cal Henderson <cal@iamcal.com>
2015-02-20 10:34:00 -0800

    what's updog?

---
+++ index.php
@@ -0,0 +1,47 @@
+<html>
+<head>
+<title>Updog</title>
+<meta name="viewport" content="width=320" />
+<meta name="viewport" content="initial-scale=1.0" />
+<meta name="viewport" content="user-scalable=false" />
+<style>
+body {
+	background-color: #eed;
+}
+
+h1 {
+	font-size: 400%;
+	margin-top: 20%;
+	text-align: center;
+	color: #333;
+}
+
+a {
+	color: #333;
+	text-decoration: none;
+}
+
+a:hover {
+	text-decoration: underline;
+}
+
+</style>
+</head>
+<body>
+
+	<h1>Updog?</h1>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-106458-20', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+</body>
+</html>
+
